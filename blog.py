11dedd6a6973c5697d1ad1a8ee01b64e563fd9e8
Mike Resoli <mike.resoli@gmail.com>
2017-11-07 14:59:43 +0000

    Yeah it seems to work

--- blog.py
+++ blog.py
@@ -1,3 +1,13 @@
+""" file for interacting with blog objects """
+import pygsheets
+
+def GetBlogs():
+    gc = pygsheets.authorize(service_file='client_secret.json', no_cache=True)
+    wks = gc.open("BlogDB").sheet1
+    rows = wks.get_all_records('', 1)
+
+    return rows
+
 class Blog(object):
     blogid = -1
     datecreated = ""
--- main.py
+++ main.py
@@ -1,44 +1,47 @@
-from flask import Flask, render_template, Response, request, flash, url_for
-import requests, json, random, os
-import pygsheets
-from oauth2client.service_account import ServiceAccountCredentials
-from blog import Blog
+""" A blog using Google Sheets as a DB, served as a Flask application """
+from flask import Flask, render_template, redirect
+from blog import Blog, GetBlogs
 from werkzeug.contrib.cache import SimpleCache
 
-app = Flask(__name__)
-app.secret_key = "dfoadhfoiawert834trjwernhfgp9werytawj"
-cache = SimpleCache()
+APP = Flask(__name__)
+APP.secret_key = "dfoadhfoiawert834trjwernhfgp9werytawj"
+CACHE = SimpleCache()
 
-@app.route('/')
+@APP.route('/')
 def index():
-	gc = pygsheets.authorize(service_file='client_secret.json', no_cache=True)
-	wks = gc.open("BlogDB").sheet1
-	blogs = cache.get('blogs')
-	data = []
-	cached = ''
+    """ the index view or blog list page """
+    blogs = CACHE.get('blogs')
+    data = []
+    cached = ''
 
-	if blogs is None:
-		cached = 'false'
-		rows = wks.get_all_records('', 1)
-		for i in rows:
-			data.append(
-				Blog(
-					blogid = i['BlogID'],
-					datecreated = i['DateCreated'],
-					title = i['Title'],
-					description = i['Description'],
-					content = i['Content'],
-					contenttype = i['ContentType'],
-					author = i['Author'],
-					tags = i['Tags']
-				)
-			)
-		cache.set('blogs', data, timeout= 5 * 60)
-	else:
-		cached = 'true'
-		data = blogs
+    if blogs is None:
+        cached = 'false'
+        rows = GetBlogs()
+        for i in rows:
+            data.append(
+                Blog(
+                    blogid = i['BlogID'],
+                    datecreated = i['DateCreated'],
+                    title = i['Title'],
+                    description = i['Description'],
+                    content = i['Content'],
+                    contenttype = i['ContentType'],
+                    author = i['Author'],
+                    tags = i['Tags']
+                )
+            )
+        CACHE.set('blogs', data, timeout= 5 * 10)
+    else:
+        cached = 'true'
+        data = blogs
 
-	return render_template('index.html', blogs=data, cached=cached)
+    return render_template('index.html', blogs=data, cached=cached)
+
+@APP.route('/clear-cache')
+def clearcache():
+    """ clears blogs cache object """
+    CACHE.set('blogs', None, timeout=1)
+    return redirect('/')
 
 if __name__ == "__main__":
-	app.run()
+    APP.run()
